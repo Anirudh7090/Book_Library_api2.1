00d9e9a9529a9ee24b308690e913cca7b08f8265
Anirudh7090 <anirudha.patil8660@gmail.com>
2025-09-10 12:30:12 +0530

    Update 816f60f33935_add_review_model_for_book_reviews.py

--- alembic/versions/816f60f33935_add_review_model_for_book_reviews.py
+++ alembic/versions/816f60f33935_add_review_model_for_book_reviews.py
@@ -10,7 +10,7 @@ from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 
-# revision identifiers, used by Alembic.
+
 revision: str = '816f60f33935'
 down_revision: Union[str, Sequence[str], None] = '57dc39be1165'
 branch_labels: Union[str, Sequence[str], None] = None
